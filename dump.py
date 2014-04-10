be9b19e12094942ba3c4f9a1426d38cab0f93613
sodamodo <lamphead@gmail.com>
2014-04-10 13:58:41 -0400

    Got rid of hand class and just put the hand in the player class. added a method to get the initial deal in the deck class which the player class can access because it has deck as a parameter. Also created a junk file to store things

---
+++ dump.py
@@ -0,0 +1,54 @@
+# class Hand:
+#     def __init__(self,deck,playernum):
+#         self.hand = []
+#         if playernum < 4:
+#             self.handsize = 5
+#         else:
+#             self.handsize = 4
+#
+#     def getHand(self):
+#         for i in range(self.handsize):
+#             self.hand.append(self.deck.deck.pop())
+#         return self.hand
+#
+#     #use to get identifiers out of card objects
+#     def get(self):
+#         return [i.id for i in self.hand ]
+#
+#     def __str__(self):
+#         return [i.id for i in self.hand ]
+
+
+
+
+        colorcount = 0
+        # the zero is just the showing player
+        #cardlist = []
+        #targetcardlist = []
+        #for card in self.players[0].hand:
+        #    cardlist.append(card.color)
+        #for card.color in self.players[targetPlayer].hand:
+        #    targetcardlist.append(card.color)
+        #matches = [x for x in targetcardlist if x in cardlist]
+               #i f cards[colorindex] in self.players[targetPlayer].hand.cards.colors[colorindex]:
+                #    print("its a match")
+
+        #print(self.players[targetPlayer].hand.color[colorindex])
+
+
+
+
+
+
+
+
+#initialize deck
+#deck = Deck()
+
+#create hands
+#hands = []
+#for i in range(playernum):
+#    hands.append(Hand(deck,playernum))
+
+#for i in hands:
+#    print(i.get())
--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 from random import shuffle
 
-playernum = 4
+
 
 #class Singleton(object):
 #  _instances = {}
@@ -8,6 +8,13 @@ playernum = 4
 #    if class_ not in class_._instances:
 #        class_._instances[class_] = super(Singleton, class_).__new__(class_, *args, **kwargs)
 #    return class_._instances[class_]
+
+playernum = 4
+
+if playernum < 4:
+    handsize = 5
+else:
+    handsize = 4
 
 
 class Card:
@@ -33,105 +40,35 @@ class Deck:
                     self.deck.append(Card(color,number))
         shuffle(self.deck)
 
+    def firstDeal(self):
+        hand = []
+        for i in range(handsize):
+            hand.append(self.deck.pop())
+
+        return [(i, j) for i, j in enumerate(hand)]
+
+
     def __str__(self):
         for i in self.deck:
             print(i)
 
-
-class Hand:
-    def __init__(self,deck,playernum):
-        self.hand = []
-        if playernum < 4:
-            handsize = 5
-        else:
-            handsize = 4
-
-        for i in range(handsize):
-            self.hand.append(deck.deck.pop())
-
-
-    #use to get identifiers out of card objects
-    def get(self):
-        return [i.id for i in self.hand ]
-
-    def __str__(self):
-        return [i.id for i in self.hand ]
-
-
-
 class Player:
-    def __init__(self,id,hand):
-        self.id = id
-        self.hand = hand
-        self.ident = { self.id : self.hand}
-
+    def __init__(self,deck):
+        self.hand = deck.firstDeal()
 
 
 class State:
-    def __init__(self,playernum):
+    def __init__(self):
         self.tokens = 8
         self.bombs = 3
         self.playerturn = 0
-        self.players = {}
         self.deck = Deck()
-        for r in range(playernum):
-            #Player(r, Hand(self.deck,playernum))
-            #print(Player.ident[r])
-            self.players[r] = Hand(self.deck,playernum)
-            print(self.players[r].get())
+        self.tehplaya = Player(self.deck)
 
-    #Players get color list from their hand which contains cards which contains colors
-    #def giveColor(self,targetPlayer,colorindex):
-    def giveColor(self):
-        colors = ['Red','White','Blue','Green','Yellow']
-        identifiedCards = []
-        givingPlayer = 2
-        targetPlayer = 4
-        givecolor = colors[2]
-        for key in self.players[targetPlayer].keys():
-            if givecolor == self.players[targetPlayer][key]:
-                identifiedCards.append(key)
+        # So I can't print out card representations yet without doing this, but Ill come back to that later
+        print(self.tehplaya.hand[0][1])
 
-        print(key)
+State()
 
 
-
-
-
-
-
-
-        colorcount = 0
-        # the zero is just the showing player
-        #cardlist = []
-        #targetcardlist = []
-        #for card in self.players[0].hand:
-        #    cardlist.append(card.color)
-        #for card.color in self.players[targetPlayer].hand:
-        #    targetcardlist.append(card.color)
-        #matches = [x for x in targetcardlist if x in cardlist]
-               #i f cards[colorindex] in self.players[targetPlayer].hand.cards.colors[colorindex]:
-                #    print("its a match")
-
-        #print(self.players[targetPlayer].hand.color[colorindex])
-
-
-
-
-
-
-
-
-#initialize deck
-#deck = Deck()
-
-#create hands
-#hands = []
-#for i in range(playernum):
-#    hands.append(Hand(deck,playernum))
-
-#for i in hands:
-#    print(i.get())
-
-State(playernum).giveColor()
 
